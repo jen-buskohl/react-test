60936fde9862afa6a56d7c928cb5a25d1ab3aa77
jen-buskohl <124739446+jen-buskohl@users.noreply.github.com>
2023-09-28 11:15:34 -0700

    Changed p to h1 and added emojis

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React is really fun!</p>
+        <h1>🥳React is really fun!🤩</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
